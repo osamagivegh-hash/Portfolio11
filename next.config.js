3cc342df36a05108aa51e2db8c454fbe249b5c38
Osama MD. Mousa <osamagivegh@gmail.com>
2026-02-10 13:21:21 +0300

    Fix: Revert partial export setting. User MUST clear Output Directory in Vercel.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export', // Removed for Vercel standard deployment
   trailingSlash: true,
   images: {
     unoptimized: true
